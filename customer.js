5e94d58874c0d387d3fd18e652c38e06219fcd20
Jonathan Grim <jonjongrim@gmail.com>
2017-04-25 17:10:25 -0400

    Implement sorting method for customers array

--- customer.js
+++ customer.js
@@ -1,8 +1,9 @@
 var customerSet = (function () {
     //private variables
     var customers = [];
-    var Customer = function (name, credit = 0.00, orderCount = 0, redemptionEligible = false) {
-        this.name = name;
+    var Customer = function (firstName, lastName, credit = 0.00, orderCount = 0, redemptionEligible = false) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.credit = credit.toFixed(2);
         this.orderCount = orderCount;
         this.redemptionEligible = redemptionEligible;
@@ -11,14 +12,17 @@ var customerSet = (function () {
     //DOM elements
     const customerTable = document.querySelector('#customerTable');
     const searchBar = document.querySelector('.search-bar');
+    const alphabetBar = document.querySelectorAll('.alphabet-letter');
 
     //Bind event listeners    
     searchBar.addEventListener('change', findMatches);    
-    searchBar.addEventListener('keyup', findMatches);    
+    searchBar.addEventListener('keyup', findMatches);
+    alphabetBar.forEach(letter => letter.addEventListener('click', findMatches));
 
     //DOM rendering
     function makeTable(matchedArray) {
         let customerArray = matchedArray || customers;
+        customerArray = sortCustomers(customerArray);
         let result = `<div class="table-row header">
             <div class="text">Name</div>
             <div class="text">Lifetime Order Count</div>
@@ -30,7 +34,7 @@ var customerSet = (function () {
         let tableRows = customerArray.map((customer, i) => {
             return `
             <div class="table-row">
-                <div class="text">${customer.name}</div>
+                <div class="text">${customer.firstName + ' ' + customer.lastName}</div>
                 <div class="text">${customer.orderCount}</div>
                 <div class="num">$${customer.credit}</div>
                 <div class="text">${(customer.redemptionEligible) ? 'Eligible' : 'Not Eligible'}</div>
@@ -44,16 +48,50 @@ var customerSet = (function () {
         customerTable.innerHTML = result + tableRows;
     }
 
+    //TODO write sort function
+    function sortCustomers(unsortedList) {
+        if (unsortedList.length == 1) {
+            return unsortedList;
+        } else {
+            let midPoint = Math.floor(unsortedList.length / 2);
+            var listA = sortCustomers(unsortedList.slice(0, midPoint));
+            var listB = sortCustomers(unsortedList.slice(midPoint));
+        }
+
+        let mergeList = [];
+        console.log(typeof listB);
+
+        while (listA.length > 0 && listB.length > 0) {
+            let result = listA[0].lastName < listB[0].lastName ? listA.shift() : listB.shift();
+            mergeList.push(result);
+        }
+
+        while (listA.length > 0) {
+            mergeList.push(listA.shift());
+        }
+        while (listB.length > 0) {
+            mergeList.push(listB.shift());
+        }
+
+        return mergeList;
+    }
+
+    function jumpToLetter() {
+        let letter = this.innerText;
+        //TODO
+        
+    }
+
     function findMatches() {
         let matchedArray = customers.filter(customer => {
             let matchValue = new RegExp(this.value, 'gi');
-            return customer.name.match(matchValue);
+            return customer.firstName.match(matchValue) || customer.lastName.match(matchValue);
         })
         makeTable(matchedArray);
     }
     
-    function newCustomer (name, credit, orderCount, redemptionEligible) {
-        return new Customer(name, credit, orderCount, redemptionEligible);
+    function newCustomer (firstName, lastName, credit, orderCount, redemptionEligible) {
+        return new Customer(firstName, lastName, credit, orderCount, redemptionEligible);
     };
 
     function addCustomer (customer) {
@@ -68,19 +106,30 @@ var customerSet = (function () {
         customer.redemptionEligible = status;
     };
 
+    function customerList() {
+        return customers;
+    }
+
     return {
+        getSortedList: sortCustomers,
         newCustomer: newCustomer,
         addCustomer: addCustomer,
         getCustomerByName: getCustomerByName,
-        makeCustomerTable: makeTable
+        makeCustomerTable: makeTable,
+        getCustomerList: customerList
     }
 })();
 
 // for temporary testing
 for (var i = 0; i < 2000; i++) {
     let person = customerSet
-        .newCustomer(`${String.fromCharCode(65 + (i % 26)) + String.fromCharCode(98 + (i % 25))}`, i, i, (i % 2) ? true : false);
+        .newCustomer(`${String.fromCharCode(65 + (i % 26)) + String.fromCharCode(98 + (i % 25))}`,
+        `${String.fromCharCode(65 + (i % 26)) + String.fromCharCode(98 + (i % 25))}`, i, i, (i % 2) ? true : false);
     customerSet.addCustomer(person);
 }
+
+var customers = customerSet.getCustomerList();
+var sortedCustomers = customerSet.getSortedList(customers);
+console.log(sortedCustomers);
 
 customerSet.makeCustomerTable();
